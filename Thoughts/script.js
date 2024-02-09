d80cd31267b334f4298274230b6e36ce22ff0508
Keng64 <119704820+Keng64@users.noreply.github.com>
2024-02-09 08:37:29 -0600

    yessir

--- Thoughts/script.js
+++ Thoughts/script.js
@@ -2,7 +2,7 @@ setInterval(loop, 5);
 
 function loop() {
   let para = document.createElement("p");
-  para.innerText = "Thoughts";
+  para.innerText = "GYAAAAAAAATT";
   para.style.position = "absolute";
   para.style.left = Math.random() * (99) + 1 + "%";
   para.style.top = Math.random() * (99) + 1 + "%";
