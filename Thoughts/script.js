a906cffb18ce08a2bdd9117ab60d8b24ab1fdf05
Keng64 <119704820+Keng64@users.noreply.github.com>
2024-02-09 09:11:18 -0600

    COLOR CHANGE

--- Thoughts/script.js
+++ Thoughts/script.js
@@ -1,19 +1,20 @@
-setInterval(loop, 5);
+setInterval(loop, 50);
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function loop() {
   let para = document.createElement("p");
-  para.innerText = "BILL NYE";
+  para.innerText = "Thoughts";
   para.style.position = "absolute";
   para.style.left = Math.random() * (99) + 1 + "%";
   para.style.top = Math.random() * (99) + 1 + "%";
   document.body.appendChild(para);
-
-  let bill_nye = document.createElement("img");
-  bill_nye.style.position = "absolute";
-  bill_nye.style.left = Math.random() * (99) + 1 + "%";
-  bill_nye.style.top = Math.random() * (99) + 1 + "%";
-  bill_nye.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/10d7e8b1-9fe1-4c41-aeb7-331f4fb188aa/de36eea-d271bd08-275a-4281-936e-766cdc80715f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzEwZDdlOGIxLTlmZTEtNGM0MS1hZWI3LTMzMWY0ZmIxODhhYVwvZGUzNmVlYS1kMjcxYmQwOC0yNzVhLTQyODEtOTM2ZS03NjZjZGM4MDcxNWYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.6rNjSNlMC85-x_k5lV5p1xujGd6uR2OimoJ1hp6gIKE";
-  bill_nye.width = "50px";
-  bill_nye.height = "50px";
-  document.body.appendChild(para);
+  para.style.color = getRandomColor();
 }
